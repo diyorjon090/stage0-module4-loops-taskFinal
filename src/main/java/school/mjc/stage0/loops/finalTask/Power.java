f32b4e54893472467d847d4a8cd16558ef6949fd
diyorjon090 <saidovdiyorbek090@gmail.com>
2022-12-22 21:55:33 +0500

    mironiy

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,8 +2,14 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
-
+        int temp = numberToPrint;
+        for (int i = 1; i < power; i++) {
+            temp = numberToPrint * temp;
+        }
+        System.out.println(temp);
     }
+
+
 
     public static void main(String[] args) {
         new Power().printPower(10, 3);
